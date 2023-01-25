cd049fd6aecdc972cb7f3e987e7c485d07f3eea8
Valeriya <lerarozhentseva@gmail.com>
2023-01-25 23:05:23 +0300

    fix bugs in drawing with brush

--- src/pages/paint/helpers/brush.ts
+++ src/pages/paint/helpers/brush.ts
@@ -1,36 +1,38 @@
 import {
-  Coordinates,
-  OnMouseDownArguments,
-  OnMouseMoveArguments,
+    Coordinates,
+    OnMouseDownArguments,
+    OnMouseMoveArguments
 } from "../../../core/interfaces/paintInterface";
 
 const brush = {
-  onMouseDown: ({
-    e,
-    context,
-    canvasOffset,
-    setIsPainting,
-  }: OnMouseDownArguments): Coordinates => {
-    setIsPainting(true);
-    if (context) {
-      context.beginPath();
-      context.moveTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
-    }
-    return { top: 0, left: 0 };
-  },
+    onMouseDown: ({
+                      e,
+                      context,
+                      canvasOffset,
+                      setIsPainting,
+                  }: OnMouseDownArguments): Coordinates => {
+        setIsPainting(true);
+        if (context) {
+            context.beginPath();
+            context.arc(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top, context.lineWidth / 1000, 0, 2 * Math.PI);
+            context.fill();
+            context.stroke();
+        }
+        return {top: 0, left: 0};
+    },
 
-  onMouseMove: ({
-    e,
-    context,
-    canvasOffset,
-    isPainting,
-  }: OnMouseMoveArguments): void => {
-    if (isPainting && context) {
-      context.lineTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
-      context.lineJoin = "round";
-      context.stroke();
-    }
-  },
+    onMouseMove: ({
+                      e,
+                      context,
+                      canvasOffset,
+                      isPainting,
+                  }: OnMouseMoveArguments): void => {
+        if (isPainting && context) {
+            context.lineTo(e.pageX - canvasOffset.left, e.pageY - canvasOffset.top);
+            context.lineJoin = "round";
+            context.stroke();
+        }
+    },
 };
 
 export default brush;
